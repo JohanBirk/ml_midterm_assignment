da869c40fbb7b4a123a4edab2317f1cc6ce423c3
JohanBirk <johanob@stud.ntnu.no>
2018-08-02 16:47:27 +0900

    Updated Problem 6 with convergence plot and plot of initial dataset

--- problem_6.py
+++ problem_6.py
@@ -5,6 +5,8 @@ from get_data_set import get_data_set
 plot_result = True
 plot_like_task = False
 same_colorbar = False
+font_size = 14
+legend_font_size = 12
 
 tol = 1e-5
 max_iter = 2000
@@ -21,11 +23,13 @@ def J_grad(A_arg, Z_arg):
 def proximal(mu_arg, lambda_arg):
     return np.max((np.zeros(np.size(mu_arg)), np.abs(mu_arg) - lambda_arg), 0)*np.sign(mu_arg)
 
-def proximal_gradient(Z, A, J, J_grad, lam, eta, tol, max_iter):
+def proximal_gradient(Z, A, A_0, J, J_grad, lam, eta, tol, max_iter):
     gradient = J_grad(A, Z)
     Z_prev = 100*Z
     iter = 0
     eta_init = eta
+    result_vec = np.zeros(1)
+    result_vec[0] = np.linalg.norm(Z - A_0)
     #while np.linalg.norm(gradient+ lam*np.linalg.norm(Z,'nuc')) > tol and iter < max_iter:
     for k in range(tot_iter):
     #while np.linalg.norm(np.nan_to_num(A) + np.isnan(A)*Z - Z_prev) > tol and iter < max_iter:
@@ -34,12 +38,12 @@ def proximal_gradient(Z, A, J, J_grad, lam, eta, tol, max_iter):
         s = proximal(s, lam * eta)
         Z_prev = np.nan_to_num(A) + np.isnan(A)*Z
         Z = np.dot(u, np.dot(np.diag(s), vh))
-        # result_vec = np.r_[result_vec, np.linalg.norm(w - w_result)]
+        result_vec = np.r_[result_vec, np.linalg.norm(np.nan_to_num(A) + np.isnan(A)*Z - A_0)]
         gradient = J_grad(A, Z)
         iter = iter + 1
-    return np.nan_to_num(A) + np.isnan(A)*Z, iter
+    return np.nan_to_num(A) + np.isnan(A)*Z, iter, result_vec
 
-def NMF(A, tol):
+def NMF(A, A_0, tol):
     # Lee and Seung's multiplicative update rule
     r = 4
     V = np.nan_to_num(A)
@@ -49,6 +53,8 @@ def NMF(A, tol):
     W = np.random.rand(m, r)
     H = np.random.rand(r, n)
     eps = 0 #1e-12 # term to avoid dividing by 0
+    result_vec = np.zeros(1)
+    result_vec[0] = np.linalg.norm(V - A_0)
     iter = 0
     for k in range(tot_iter):
     #while np.linalg.norm(V - V_prev) > tol and iter < max_iter:
@@ -59,8 +65,9 @@ def NMF(A, tol):
         #H = H*(np.sum(W**2,axis=0)[:, None]**0.5)
         V_prev = V
         V = V*(~V_mem) + np.matmul(W,H)*V_mem
+        result_vec = np.r_[result_vec, np.linalg.norm(V - A_0)]
         iter += 1
-    return np.nan_to_num(A) + np.isnan(A)*np.matmul(W,H), iter
+    return np.nan_to_num(A) + np.isnan(A)*np.matmul(W,H), iter, result_vec
 
 
 A, A_0 = get_data_set(3)
@@ -68,8 +75,8 @@ A, A_0 = get_data_set(3)
 n, r = np.shape(A)
 Z_init = np.random.rand(n, r)
 
-Z_NMF, iter_nmf = NMF(A, tol)
-Z, iter_prox = proximal_gradient(Z_init, A, J, J_grad, lam, eta, tol, max_iter)
+Z_NMF, iter_nmf, result_nmf = NMF(A, A_0, tol)
+Z, iter_prox, result_prox = proximal_gradient(Z_init, A, A_0, J, J_grad, lam, eta, tol, max_iter)
 print('Number of iterations')
 print(("Proximal gradient iterations: %i") % iter_prox)
 print(("NMF iterations: %i") % iter_nmf)
@@ -78,7 +85,7 @@ print(("NMF iterations: %i") % iter_nmf)
 
 plt.figure()
 
-plt.subplot(311)
+plt.subplot(221)
 plt.imshow(A)
 plt.colorbar()
 if plot_like_task:
@@ -86,18 +93,33 @@ if plot_like_task:
     current_cmap.set_bad(color='darkslateblue')
 plt.title("Dataset")
 
-plt.subplot(312)
+plt.subplot(222)
+plt.imshow(A_0)
+plt.colorbar()
+if plot_like_task:
+    current_cmap = plt.cm.get_cmap()
+    current_cmap.set_bad(color='darkslateblue')
+plt.title("Original dataset")
+
+plt.subplot(223)
 plt.imshow(Z)  # , interpolation='nearest', vmin=np.nan)???
 plt.colorbar()
 plt.title("Recoverey method 1")
 plt.subplots_adjust(hspace=0.3)
 
-plt.subplot(313)
+plt.subplot(224)
 plt.imshow(Z_NMF)  # , interpolation='nearest', vmin=np.nan)???
 plt.colorbar()
 plt.title("Recovery method 2")
 plt.subplots_adjust(hspace=0.3)
 
+plt.figure()
+
+plt.semilogy(result_prox[1:100], '--', label='Proximal gradient')
+plt.semilogy(result_nmf[1:100], '--', label='NMF')
+plt.xlabel("No. of iterations", fontsize=font_size)
+plt.ylabel("$\|| \hat{A} - A_{0} \|| $", fontsize=font_size)
+plt.legend(fontsize=legend_font_size)
 
 plt.show(block=False)
 plt.show()
